38bc92d97a6936d458099fc4fe993c8f620d4647
Christopher McCooey <ccmccooey@uri.edu>
2020-02-12 12:59:51 -0500

    Added a sample linked list class
    - added sample linked list

---
+++ lab-03/chris_inclass/SampleLinkedList/LinkedList.cpp
@@ -0,0 +1,66 @@
+#include "LinkedList.h"
+
+//construct an empty list
+LinkedList::LinkedList()
+{
+    head = nullptr;
+    tail = nullptr;
+    count = 0;
+}
+LinkedList::~LinkedList()
+{
+
+}
+
+void LinkedList::append(double value)
+{
+    //case 1 (list is empty)
+    if (count == 0)
+    {
+        Node *my_node = new Node();
+        my_node->data = value;
+        my_node->next = nullptr;
+        head = my_node;
+        tail = my_node;
+        count = 1;
+    }
+    //case 2 (list is not empty)
+    else
+    {
+        Node *my_node = new Node(); //add to the end of the list
+        my_node->data = value;
+        my_node->next = nullptr;
+        tail->next = my_node;
+        tail = my_node;
+        count += 1; //increase count by 1
+    }
+}
+
+void LinkedList::removeLast()
+{
+    if (count > 0) //if the list is not empty remove last item otherwise do nothing
+    {
+        if (count == 1) //list has exactly one item in it
+        {
+            delete head;
+            head = nullptr;
+            tail = nullptr;
+            count = 0;
+        }
+        else //list has at least 2 items in it
+        {
+            Node* current = head;
+            Node* previous = head;
+            while (current != tail) //go to the second to last item in the list
+            {
+                previous = current;
+                current = current->next;
+            }
+
+            //previous will point to the second to last item in the list
+            previous->next = nullptr;
+            delete tail;
+            tail = previous;
+        }
+    }
+}
---
+++ lab-03/chris_inclass/SampleLinkedList/LinkedList.h
@@ -0,0 +1,32 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+class Node
+{
+public:
+    Node* next;
+    double data;
+};
+
+class LinkedList
+{
+private:
+    Node* head;
+    Node* tail;
+    int count;
+
+public:
+    LinkedList(); //create an empty list
+    ~LinkedList(); //delete linked list
+
+    void append(double value);
+    void prepend(double value);
+    void insert(int index, double value);
+    void removeFirst();
+    void removeLast();
+    void removeAt(int index);
+    void removeFirstOf(double value);
+    double get(int index);
+};
+
+#endif // LINKEDLIST_H
